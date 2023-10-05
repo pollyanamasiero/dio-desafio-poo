0ad0eb3d6ba2fe88b672c5e04666d6e0967bb713
Pollyana Masiero Torquato <113457708+pollyanamasiero@users.noreply.github.com>
2023-10-05 19:00:37 -0300

    Add files via upload

---
+++ Desafio/src/MusicPlayer.java
@@ -0,0 +1,5 @@
+public interface MusicPlayer {
+    void tocar();
+    void pausar();
+    void selecionarMusica(String musica);
+  }
---
+++ Desafio/src/iPhone.java
@@ -0,0 +1,50 @@
+public class iPhone implements MusicPlayer, AparelhoTelefonico, NavegadorInternet {
+
+    // Implementação dos métodos da interface ReprodutorMusical
+    @Override
+    public void tocar() {
+        // Implementação
+    }
+
+    @Override
+    public void pausar() {
+        // Implementação
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        // Implementação
+    }
+
+    // Implementação dos métodos da interface AparelhoTelefonico
+    @Override
+    public void ligar(String numero) {
+        // Implementação
+    }
+
+    @Override
+    public void atender() {
+        // Implementação
+    }
+
+    @Override
+    public void iniciarConversaVoz() {
+        // Implementação
+    }
+
+    // Implementação dos métodos da interface NavegadorInternet
+    @Override
+    public void exibirPagina(String url) {
+        // Implementação
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        // Implementação
+    }
+
+    @Override
+    public void atualizarPagina() {
+        // Implementação
+    }
+}
